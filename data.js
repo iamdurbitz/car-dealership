5fe8777596c9f2f7d169a8655ef533d592baf7d9
iamdurbitz <tarikaytek@icloud.com>
2026-09-14 17:30:28 +0300

    Add more data

--- data.js
+++ data.js
@@ -38,5 +38,365 @@ export const cars = [
     year: 2022,
     price: 110000,
     color: "gray"
+  },
+  {
+    id: 6,
+    brand: "Mercedes-Benz",
+    model: "AMG GT",
+    year: 2021,
+    price: 115000,
+    color: "black"
+  },
+  {
+    id: 7,
+    brand: "Lamborghini",
+    model: "Huracan",
+    year: 2023,
+    price: 245000,
+    color: "yellow"
+  },
+  {
+    id: 8,
+    brand: "Ferrari",
+    model: "488 GTB",
+    year: 2020,
+    price: 280000,
+    color: "red"
+  },
+  {
+    id: 9,
+    brand: "McLaren",
+    model: "720S",
+    year: 2022,
+    price: 310000,
+    color: "orange"
+  },
+  {
+    id: 10,
+    brand: "Toyota",
+    model: "GR Supra",
+    year: 2021,
+    price: 58000,
+    color: "white"
+  },
+  {
+    id: 11,
+    brand: "Nissan",
+    model: "GT-R",
+    year: 2020,
+    price: 120000,
+    color: "blue"
+  },
+  {
+    id: 12,
+    brand: "Honda",
+    model: "Civic Type R",
+    year: 2023,
+    price: 45000,
+    color: "red"
+  },
+  {
+    id: 13,
+    brand: "Volkswagen",
+    model: "Golf R",
+    year: 2022,
+    price: 48000,
+    color: "blue"
+  },
+  {
+    id: 14,
+    brand: "Tesla",
+    model: "Model S Plaid",
+    year: 2023,
+    price: 105000,
+    color: "white"
+  },
+  {
+    id: 15,
+    brand: "Ford",
+    model: "F-150 Raptor",
+    year: 2022,
+    price: 78000,
+    color: "gray"
+  },
+  {
+    id: 16,
+    brand: "Dodge",
+    model: "Challenger SRT Hellcat",
+    year: 2021,
+    price: 72000,
+    color: "black"
+  },
+  {
+    id: 17,
+    brand: "Chevrolet",
+    model: "Camaro SS",
+    year: 2020,
+    price: 49000,
+    color: "yellow"
+  },
+  {
+    id: 18,
+    brand: "Subaru",
+    model: "WRX STI",
+    year: 2021,
+    price: 42000,
+    color: "blue"
+  },
+  {
+    id: 19,
+    brand: "Mitsubishi",
+    model: "Lancer Evolution X",
+    year: 2019,
+    price: 38000,
+    color: "white"
+  },
+  {
+    id: 20,
+    brand: "Mazda",
+    model: "MX-5 Miata",
+    year: 2022,
+    price: 32000,
+    color: "red"
+  },
+  {
+    id: 21,
+    brand: "Lexus",
+    model: "LC 500",
+    year: 2021,
+    price: 98000,
+    color: "gray"
+  },
+  {
+    id: 22,
+    brand: "Infiniti",
+    model: "Q60 Red Sport",
+    year: 2020,
+    price: 55000,
+    color: "black"
+  },
+  {
+    id: 23,
+    brand: "Jaguar",
+    model: "F-Type R",
+    year: 2022,
+    price: 105000,
+    color: "green"
+  },
+  {
+    id: 24,
+    brand: "Aston Martin",
+    model: "Vantage",
+    year: 2021,
+    price: 145000,
+    color: "green"
+  },
+  {
+    id: 25,
+    brand: "Bentley",
+    model: "Continental GT",
+    year: 2023,
+    price: 230000,
+    color: "black"
+  },
+  {
+    id: 26,
+    brand: "Rolls-Royce",
+    model: "Wraith",
+    year: 2020,
+    price: 320000,
+    color: "white"
+  },
+  {
+    id: 27,
+    brand: "Maserati",
+    model: "MC20",
+    year: 2022,
+    price: 220000,
+    color: "blue"
+  },
+  {
+    id: 28,
+    brand: "Alfa Romeo",
+    model: "Giulia Quadrifoglio",
+    year: 2021,
+    price: 85000,
+    color: "red"
+  },
+  {
+    id: 29,
+    brand: "Volvo",
+    model: "S60 Polestar",
+    year: 2022,
+    price: 65000,
+    color: "gray"
+  },
+  {
+    id: 30,
+    brand: "Genesis",
+    model: "G80",
+    year: 2023,
+    price: 62000,
+    color: "black"
+  },
+  {
+    id: 31,
+    brand: "Hyundai",
+    model: "i30 N",
+    year: 2021,
+    price: 39000,
+    color: "blue"
+  },
+  {
+    id: 32,
+    brand: "Kia",
+    model: "Stinger GT",
+    year: 2022,
+    price: 47000,
+    color: "orange"
+  },
+  {
+    id: 33,
+    brand: "Renault",
+    model: "Megane RS",
+    year: 2020,
+    price: 35000,
+    color: "yellow"
+  },
+  {
+    id: 34,
+    brand: "Peugeot",
+    model: "508 PSE",
+    year: 2022,
+    price: 52000,
+    color: "green"
+  },
+  {
+    id: 35,
+    brand: "Mercedes-Benz",
+    model: "C63 AMG",
+    year: 2020,
+    price: 92000,
+    color: "white"
+  },
+  {
+    id: 36,
+    brand: "BMW",
+    model: "M3 Competition",
+    year: 2023,
+    price: 95000,
+    color: "green"
+  },
+  {
+    id: 37,
+    brand: "Audi",
+    model: "RS3",
+    year: 2021,
+    price: 60000,
+    color: "red"
+  },
+  {
+    id: 38,
+    brand: "Porsche",
+    model: "Cayenne Turbo",
+    year: 2023,
+    price: 145000,
+    color: "gray"
+  },
+  {
+    id: 39,
+    brand: "Toyota",
+    model: "GR Yaris",
+    year: 2022,
+    price: 41000,
+    color: "white"
+  },
+  {
+    id: 40,
+    brand: "Nissan",
+    model: "370Z",
+    year: 2019,
+    price: 35000,
+    color: "yellow"
+  },
+  {
+    id: 41,
+    brand: "Ford",
+    model: "Focus RS",
+    year: 2019,
+    price: 44000,
+    color: "blue"
+  },
+  {
+    id: 42,
+    brand: "Chevrolet",
+    model: "Tahoe",
+    year: 2022,
+    price: 68000,
+    color: "black"
+  },
+  {
+    id: 43,
+    brand: "Jeep",
+    model: "Grand Cherokee Trackhawk",
+    year: 2021,
+    price: 90000,
+    color: "gray"
+  },
+  {
+    id: 44,
+    brand: "Land Rover",
+    model: "Range Rover Sport",
+    year: 2023,
+    price: 125000,
+    color: "green"
+  },
+  {
+    id: 45,
+    brand: "Mercedes-Benz",
+    model: "G63 AMG",
+    year: 2022,
+    price: 180000,
+    color: "black"
+  },
+  {
+    id: 46,
+    brand: "BMW",
+    model: "X5 M Competition",
+    year: 2023,
+    price: 125000,
+    color: "white"
+  },
+  {
+    id: 47,
+    brand: "Audi",
+    model: "RS Q8",
+    year: 2022,
+    price: 135000,
+    color: "blue"
+  },
+  {
+    id: 48,
+    brand: "Lamborghini",
+    model: "Urus",
+    year: 2023,
+    price: 240000,
+    color: "black"
+  },
+  {
+    id: 49,
+    brand: "Porsche",
+    model: "Taycan Turbo S",
+    year: 2023,
+    price: 195000,
+    color: "white"
+  },
+  {
+    id: 50,
+    brand: "Tesla",
+    model: "Model 3 Performance",
+    year: 2022,
+    price: 55000,
+    color: "red"
   }
 ]
